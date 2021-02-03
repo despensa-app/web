5926f0ddcb6514ada36effd5f0ac91b07fa08e2d
Nicolás Marulanda P <nicomaru@outlook.com>
2021-02-03 21:21:20 +0100

    Componente Index.jsx - Implementación básica de "react-router-dom".

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,3 +1,8 @@
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
 import Aside from './common/Aside';
 import Footer from './common/Footer';
 import Content from "./common/Content";
@@ -8,13 +13,40 @@ const Index = () => {
     const version = "1.0.0";
 
     return (
-        <>
+        <Router>
             <Nav/>
             <Aside/>
-            {/*<Content/>*/}
-            <Product/>
+            <Switch>
+                <Route path="/products/:productId">
+                    <Product/>
+                </Route>
+                <Route path="/products/form">
+                    <Content/>
+                </Route>
+                <Route path="/products">
+                    <Content/>
+                </Route>
+                <Route path="/unit-types/:unitTypeId">
+                    <Content/>
+                </Route>
+                <Route path="/unit-types">
+                    <Content/>
+                </Route>
+                <Route path="/shopping-list/:shoppingListId">
+                    <Content/>
+                </Route>
+                <Route path="/shopping-list/form">
+                    <Content/>
+                </Route>
+                <Route path="/shopping-list">
+                    <Content/>
+                </Route>
+                <Route path="/">
+                    <Content/>
+                </Route>
+            </Switch>
             <Footer version={version}/>
-        </>
+        </Router>
     );
 };
 
