ac3f4fcf95c8fd716e9dbaef8a6aee6d707b09cd
Nicolás Marulanda P <nicomaru@outlook.com>
2021-02-07 15:44:33 +0100

    Componente BreadCrumb - Se vuelve a crear, con una implementación diferente.

--- src/components/common/BreadcrumbItem.jsx
+++ src/components/common/BreadcrumbItem.jsx
@@ -1,11 +1,32 @@
 import {Link} from "react-router-dom";
+import clsx from "clsx";
 
-const BreadcrumbItem = ({active, children, href}) => {
+const BreadcrumbItem = ({active, url, label, preIconClassName}) => {
+    const labelRender = (
+        <>
+            {preIconClassName && <span className={preIconClassName}> </span>  }
+            {label}
+        </>
+    );
+
     return (
-        <li className={`breadcrumb-item${active ? ' active' : ''}`}>
-            {!active && href ? <Link to={href}>{children}</Link> : children}
+        <li className={clsx('breadcrumb-item', {active})}>
+            {active ? labelRender : <Link to={url || '#'}>{labelRender}</Link>}
         </li>
     );
 };
 
-export default BreadcrumbItem;
+const BreadCrumb = ({home, items}) => {
+    return (
+        <ol className="breadcrumb float-sm-right">
+            <BreadcrumbItem {...home}/>
+            {
+                items.map((item, i) => (
+                    <BreadcrumbItem key={`breadcrumb-item-${item.label}-${i}`} {...item}/>
+                ))
+            }
+        </ol>
+    );
+}
+
+export default BreadCrumb;
