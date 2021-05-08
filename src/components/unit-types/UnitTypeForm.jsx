621ab622915af1829e20afdb7461036ba205567b
Nicolás Marulanda P <nicomaru@outlook.com>
2021-05-08 11:43:01 +0200

    Componente UnitTypes y Form - Implementación básica. Obtener datos, crear, actualizar y eliminar.

---
+++ src/components/unit-types/UnitTypeForm.jsx
@@ -0,0 +1,99 @@
+import {useContext, useState, useEffect} from "react";
+import {useHistory, useParams} from "react-router-dom";
+import Card from "../common/Card";
+import {LoadingProcessScreenContext} from "../../App";
+
+const UnitTypeForm = ({onActionSubmit}) => {
+
+    const url = 'http://despensa-app.api/api/unit-types';
+
+    const [unitTypeName, setUnitTypeName] = useState('');
+
+    const loadingProcessScreen = useContext(LoadingProcessScreenContext);
+
+    const {unitTypeId} = useParams();
+
+    const history = useHistory();
+
+    useEffect(() => {
+        setUnitTypeName('');
+
+        if (!unitTypeId) {
+            return;
+        }
+
+        loadingProcessScreen.show();
+        fetch(`${url}/${unitTypeId}`)
+            .then(response => response.json())
+            .then(data => {
+                setUnitTypeName(data.data.name);
+            })
+            .catch(error => console.log(error))
+            .finally(loadingProcessScreen.hide);
+    }, [unitTypeId]);
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        loadingProcessScreen.show();
+
+        if (unitTypeId) {
+            update();
+        } else {
+            create();
+        }
+    };
+
+    const create = () => {
+        const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                name: unitTypeName,
+            })
+        };
+
+        fetch(url, requestOptions)
+            .then(response => response.json())
+            .then(data => history.push(`/unit-types/${data.data.id}`))
+            .catch(error => console.log(error))
+            .finally(onActionSubmit);
+    }
+
+    const update = () => {
+        const requestOptions = {
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                name: unitTypeName,
+            })
+        };
+
+        fetch(`${url}/${unitTypeId}`, requestOptions)
+            .catch(error => console.log(error))
+            .finally(onActionSubmit)
+    }
+
+
+    return (
+        <Card>
+            <form className="row" onSubmit={onSubmit}>
+                <div className="col-sm-10">
+                    <div className="row">
+                        <label htmlFor="unit_type_name" className="col-sm-2 col-form-label">Nombre</label>
+                        <div className="col-sm-10">
+                            <input type="text" id="unit_type_name" className="form-control" value={unitTypeName} onChange={(e) => setUnitTypeName(e.target.value)}/>
+                        </div>
+                    </div>
+                    <br className="d-block d-sm-none"/>
+                </div>
+                <div className="col-sm-2">
+                    <button className="btn btn-primary btn-block">
+                        {unitTypeId ? 'Modificar' : 'Crear'}
+                    </button>
+                </div>
+            </form>
+        </Card>
+    );
+};
+
+export default UnitTypeForm
---
+++ src/components/unit-types/UnitTypes.jsx
@@ -0,0 +1,166 @@
+import {useContext, useState, useEffect} from "react";
+import Content from "../common/Content";
+import Card from "../common/Card";
+import {LoadingProcessScreenContext} from "../../App";
+import Pagination from "../common/Pagination";
+import UnitTypeForm from "./UnitTypeForm";
+import {Link, useParams} from "react-router-dom";
+
+const UnitTypes = () => {
+
+    const urlBase = 'http://despensa-app.api/api/unit-types';
+
+    const breadcrumbItems = {
+        home: {
+            preIconClassName: 'fas fa-home',
+            url: '/'
+        },
+        items:
+            [
+                {
+                    label: 'Tipo de unidades',
+                    active: true
+                }
+            ]
+    };
+
+    const unitTypesData = {
+        "data": [
+            {
+                "id": 46,
+                "name": "perferendis",
+                "created_at": 1612031857,
+                "updated_at": 1612031857
+            }
+        ],
+        "links": {
+            "first": "http://despensa-app.api/api/unit-types?page=1",
+            "last": "http://despensa-app.api/api/unit-types?page=67",
+            "prev": "http://despensa-app.api/api/unit-types?page=3",
+            "next": "http://despensa-app.api/api/unit-types?page=5"
+        },
+        "meta": {
+            "current_page": 4,
+            "from": 46,
+            "last_page": 67,
+            "links": [
+                {
+                    "url": "http://despensa-app.api/api/unit-types?page=3",
+                    "label": "&laquo; Previous",
+                    "active": false
+                }
+            ],
+            "path": "http://despensa-app.api/api/unit-types",
+            "per_page": 15,
+            "to": 60,
+            "total": 1000
+        }
+    }
+
+    const [unitTypes, setUnitTypes] = useState(unitTypesData);
+
+    const [url, setUrl] = useState(urlBase);
+
+    const loadingProcessScreen = useContext(LoadingProcessScreenContext);
+
+    const {unitTypeId} = useParams();
+
+    useEffect(() => {
+        initData();
+    }, [url]);
+
+    const initData = () => {
+        loadingProcessScreen.show();
+        fetch(url)
+            .then(response => response.json())
+            .then(data => setUnitTypes(data))
+            .catch(error => console.log(error))
+            .finally(loadingProcessScreen.hide);
+    };
+
+    const PageHeader = () => (
+        <>
+            <span>Tipo de unidades de productos</span>
+            {unitTypeId && <Link to="/unit-types" className="btn btn-success">
+                <i className="fas fa-plus"/>
+            </Link>}
+        </>
+    );
+
+    const onPageClick = (url) => {
+        loadingProcessScreen.show();
+        setUrl(url);
+    };
+
+    const onActionSubmit = () => {
+        initData();
+    }
+
+    const onClickDelete = (id) => {
+        const requestOptions = {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        };
+
+        loadingProcessScreen.show();
+        fetch(`${urlBase}/${id}`, requestOptions)
+            .then(response => {
+                if (response.ok) {
+                    initData();
+                } else {
+                    loadingProcessScreen.hide();
+                    return response.json()
+                }
+            })
+            .then(response => {
+                //TODO: mostrar un mensaje en pantalla.
+                if (response && response.error) {
+                    console.log(response.error);
+                }
+            })
+            .catch(error => console.log(error));
+    }
+
+    return (
+        <Content pageHeader={<PageHeader/>} breadcrumbItems={breadcrumbItems}>
+            <UnitTypeForm onActionSubmit={onActionSubmit}/>
+            <Card className="table-responsive">
+                <table className="table table-hover">
+                    <thead>
+                        <tr>
+                            <th/>
+                            <th>Nombre</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            unitTypes.data.map((unitType, i) => (
+                                <tr key={`unit-type-${i}`}>
+                                    <td className="w-25 td-btn-actions">
+                                        <div className="btn-group">
+                                            <Link to={`/unit-types/${unitType.id}`} className="btn btn-primary btn-sm" href="/">
+                                                <i className="fas fa-edit"/>
+                                            </Link>
+                                            <button className="btn btn-danger btn-sm" onClick={(e) => onClickDelete(unitType.id)}>
+                                                <i className="far fa-trash-alt"/>
+                                            </button>
+                                        </div>
+                                    </td>
+                                    <td>{unitType.name}</td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+                <Pagination
+                    links={unitTypes.meta.links}
+                    onClick={onPageClick}
+                    nameKey="unit-types"/>
+            </Card>
+        </Content>
+    );
+};
+
+export default UnitTypes;
