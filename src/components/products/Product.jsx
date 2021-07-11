35af50c13cb4f14bad75ed5aac83a9473a0bf3af
Nicolás Marulanda P <nicomaru@outlook.com>
2021-07-11 02:43:51 +0200

    Product.jsx - Refactorización. Uso de "ShowMessagesContext", "breadCrumbApp", "ProductsRC" y objetos json.

--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -1,174 +1,78 @@
 import {useState, useEffect, useContext} from "react";
-import {Link, useParams} from "react-router-dom";
+import {Link, useHistory, useParams} from "react-router-dom";
 import Content from "../common/Content";
 import Card from "../common/Card";
 import Pagination from "../common/Pagination";
 import CardProductShopping from "./CardProductShoppin";
-import {LoadingProcessScreenContext} from "../../App";
+import {LoadingProcessScreenContext, ShowMessagesContext} from "../../App";
+import productInitState from "../../assests/responses/products.json";
+import BreadCrumbApp from "../../common/breadCrumbApp";
+import productsShoppingListInitState from "../../assests/responses/products-shopping-list.json"
+import ProductsRC from "../../services/ProductsRC";
+import routes from "../../assests/routes.json";
 
 const Product = () => {
-
-    const url = 'http://despensa-app.api/api/products';
 
     const urlRelationship = 'http://despensa-app.api/api/products-shopping-list';
 
     const {productId} = useParams();
 
-    const productData = {
-        "data": {
-            "id": 45,
-            "name": "totam",
-            "price": 7087.57,
-            "img_url": "https://via.placeholder.com/640x480.png/000055?text=quidem",
-            "calories": 725.32,
-            "description": "Dolorem debitis perferendis vitae saepe vel fuga. In architecto sed nisi delectus. Praesentium ut qui ab laboriosam est vero facere.",
-            "created_at": 1610219098,
-            "updated_at": 1610219098,
-            "product_shopping_list": [
-                {
-                    "units_per_product": 10,
-                    "total_calories": 7253.2,
-                    "total_price": 70875.7,
-                    "product_id": 45,
-                    "shopping_list_id": 25,
-                    "unit_type_id": 29,
-                    "unit_type": {
-                        "id": 29,
-                        "name": "tempore",
-                        "created_at": 1610219098,
-                        "updated_at": 1610219098
-                    },
-                    "shopping_list": {
-                        "id": 25,
-                        "name": "asperiores",
-                        "total_calories": 415411.34,
-                        "total_price": 541168.29,
-                        "created_at": 1610219098,
-                        "updated_at": 1610219099
-                    },
-                    "product": {
-                        "id": 45,
-                        "name": "totam",
-                        "price": 7087.57,
-                        "img_url": "https://via.placeholder.com/640x480.png/000055?text=quidem",
-                        "calories": 725.32,
-                        "description": "Dolorem debitis perferendis vitae saepe vel fuga. In architecto sed nisi delectus. Praesentium ut qui ab laboriosam est vero facere.",
-                        "created_at": 1610219098,
-                        "updated_at": 1610219098
-                    }
-                }
-            ]
+    const [product, setProduct] = useState(productInitState);
+
+    const breadcrumbItems = BreadCrumbApp.product([
+        {
+            label: product.data.name,
+            active: true
         }
-    };
+    ]);
 
-    const shoppingListData = {
-        "data": [
-            {
-                "units_per_product": 74,
-                "total_calories": 122805.96,
-                "total_price": 28474.46,
-                "product_id": 841,
-                "shopping_list_id": 130,
-                "unit_type_id": 835,
-                "unit_type": {
-                    "id": 835,
-                    "name": "et",
-                    "created_at": 1612031860,
-                    "updated_at": 1612031860
-                },
-                "shopping_list": {
-                    "id": 130,
-                    "name": "libero",
-                    "total_calories": 84489433.41,
-                    "total_price": 84604481.57,
-                    "created_at": 1612031866,
-                    "updated_at": 1612031986
-                },
-                "product": {
-                    "id": 841,
-                    "name": "architecto",
-                    "price": 384.79,
-                    "img_url": "https://via.placeholder.com/640x480.png/00aa00?text=eaque",
-                    "calories": 1659.54,
-                    "description": "Quae quia commodi est officia. Ducimus beatae sit cumque in quis reprehenderit est incidunt. Ducimus consequuntur nobis sit cupiditate porro porro ut.",
-                    "created_at": 1612031865,
-                    "updated_at": 1612031865
-                }
-            }
-        ],
-        "links": {
-            "first": "http://despensa-app.api/api/products/841/shopping-list?page=1",
-            "last": "http://despensa-app.api/api/products/841/shopping-list?page=7",
-            "prev": null,
-            "next": "http://despensa-app.api/api/products/841/shopping-list?page=2"
-        },
-        "meta": {
-            "current_page": 1,
-            "from": 1,
-            "last_page": 7,
-            "links": [
-                {
-                    "url": null,
-                    "label": "&laquo; Previous",
-                    "active": false
-                }
-            ],
-            "path": "http://despensa-app.api/api/products/841/shopping-list",
-            "per_page": 15,
-            "to": 15,
-            "total": 102
-        }
-    };
-
-    const [product, setProduct] = useState(productData);
-
-    const breadcrumbItems = {
-        home: {
-            preIconClassName: 'fas fa-home',
-            url: '/'
-        },
-        items:
-            [
-                {
-                    label: 'Productos',
-                    url: '/products'
-                },
-                {
-                    label: product.data.name,
-                    active: true
-                }
-            ]
-    };
-
-    const [shoppingList, setShoppingList] = useState(shoppingListData)
+    const [shoppingList, setShoppingList] = useState(productsShoppingListInitState)
 
     const [shoppingListUrlPage, setShoppingListUrlPage] = useState('');
 
     const loadingProcessScreen = useContext(LoadingProcessScreenContext);
 
+    const showMessage = useContext(ShowMessagesContext);
+
+    const history = useHistory();
+
     useEffect(() => {
-        loadingProcessScreen.show();
-        fetch(`${url}/${productId}`)
-            .then(response => response.json())
-            .then(data => setProduct(data))
-            .catch(error => console.log(error));
-        fetch(`${url}/${productId}/shopping-list`)
-            .then(response => response.json())
-            .then(data => setShoppingList(data))
-            .catch(error => console.log(error))
-            .finally(loadingProcessScreen.hide);
+        initData();
     }, [productId]);
 
-    const onPageClick = (url) => {
-        url = url ? url : `${url}/${productId}/shopping-list`;
+    const initData = () => {
+        loadingProcessScreen.show();
+        ProductsRC.get({
+            id: productId,
+            success: (data) => {
+                setProduct(data);
+                initDataShoppingList();
+            },
+            error: (data) => {
+                showMessage.error(data.error);
+                history.push(ProductsRC.getPath());
+            }
+        });
+    };
 
+    const initDataShoppingList = (url = null) => {
+        const path = ProductsRC.getPath([productId, 'shopping-list']);
+        const uri = url ? url : routes.host + path;
+
+        ProductsRC.get({
+            uri,
+            success: (data) => {
+                setShoppingList(data);
+            },
+            error: (data) => showMessage.error(data.error),
+            final: loadingProcessScreen.hide
+        });
+    };
+
+    const onPageClick = (url) => {
         loadingProcessScreen.show();
         setShoppingListUrlPage(url);
-        fetch(url)
-            .then(response => response.json())
-            .then(data => setShoppingList(data))
-            .catch(error => console.log(error))
-            .finally(loadingProcessScreen.hide);
+        initDataShoppingList(url);
     };
 
     const onRelationshipDelete = ({product_id, shopping_list_id, unit_type_id}) => {
@@ -197,7 +101,7 @@ const Product = () => {
     const PageHeader = () => (
         <>
             <span>{product.data.name} </span>
-            <Link to={`/products/form/${productId}`} className="btn btn-primary">
+            <Link to={ProductsRC.getPath([productId, 'form'])} className="btn btn-primary">
                 <i className="fas fa-edit"/>
             </Link>
             <Link to="/" className="btn btn-danger">
