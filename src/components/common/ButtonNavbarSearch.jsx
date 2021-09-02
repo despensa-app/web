1df857b470c8c949efd3073e37d991f30ff4225c
Nicolás Marulanda P <nicomaru@outlook.com>
2021-09-02 02:06:11 +0200

    Componentes comunes - ButtonNavbarSearch - Es principalmente para ser usado en el menu de navegación.

---
+++ src/components/common/ButtonNavbarSearch.jsx
@@ -0,0 +1,9 @@
+import Button from "./button/Button";
+
+const ButtonNavbarSearch = ({onClick}) => (
+    <Button onClick={onClick}>
+        <i className="fas fa-search"/>
+    </Button>
+);
+
+export default ButtonNavbarSearch;
