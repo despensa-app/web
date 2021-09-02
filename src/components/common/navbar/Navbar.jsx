8a9ff863ed07ec088121c6725769043002567cf3
Nicolás Marulanda P <nicomaru@outlook.com>
2021-09-02 02:13:02 +0200

    Componente Navbar - NavbarHandleContext - Se implementa el uso del contexto "NavbarHandleContext", para mostrar los items.

--- src/components/common/navbar/Navbar.jsx
+++ src/components/common/navbar/Navbar.jsx
@@ -1,7 +1,12 @@
 import NavbarNavItem from "./NavbarNavItem";
 import NavbarNav from "./NavbarNav";
+import {useContext} from "react";
+import {NavbarHandleContext} from "../../../App";
 
 const Navbar = () => {
+
+    const {items} = useContext(NavbarHandleContext);
+
     return (
         <nav className="main-header navbar navbar-expand fixed-bottom navbar-white navbar-light d-flex justify-content-between">
             <NavbarNav>
@@ -12,19 +17,22 @@ const Navbar = () => {
                 </NavbarNavItem>
             </NavbarNav>
             <NavbarNav>
-                <NavbarNavItem>
-                    <button className="btn btn-success" type="button">
-                        <i className="fas fa-plus pr-1"/>
-                        <span>Nuevo</span>
-                    </button>
-                </NavbarNavItem>
+                {
+                    items.middle.map((value, i) => (
+                        <NavbarNavItem key={`navbar-nav-item-middle-${i}`}>
+                            {value}
+                        </NavbarNavItem>
+                    ))
+                }
             </NavbarNav>
             <NavbarNav>
-                <NavbarNavItem>
-                    <button className="btn" type="button">
-                        <i className="fas fa-search"/>
-                    </button>
-                </NavbarNavItem>
+                {
+                    items.right.map((value, i) => (
+                        <NavbarNavItem key={`navbar-nav-item-right-${i}`}>
+                            {value}
+                        </NavbarNavItem>
+                    ))
+                }
             </NavbarNav>
         </nav>
     );
