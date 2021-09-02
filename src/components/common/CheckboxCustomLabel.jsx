329f1a8862fbbeb99fd014a211936dadc99d9fe5
Nicolás Marulanda P <nicomaru@outlook.com>
2021-09-02 23:25:57 +0200

    CheckboxCustomLabel - Refactorización. Ahora en lugar de usar un prop "check" que indique si ha seleccionado o no el input, se utiliza el evento "onClick" y un estado, para determinar cuando esta check el componente.

--- src/components/common/CheckboxCustomLabel.jsx
+++ src/components/common/CheckboxCustomLabel.jsx
@@ -1,11 +1,20 @@
 import clsx from "clsx";
+import {useState} from "react";
 
-const CheckboxCustomLabel = ({id, value, check = false}) => {
+const CheckboxCustomLabel = ({id, value, onClick}) => {
+
+    const [isCheck, setIsCheck] = useState(false);
+
+    const onClickHandle = () => {
+        setIsCheck(!isCheck);
+        onClick();
+    }
+
     return (
-        <div className="checkbox-custom-label">
+        <div className="checkbox-custom-label" onClick={onClickHandle}>
             <input className="d-none" type="checkbox" id={id} value={value}/>
-            <label className={clsx({check: check})} htmlFor={id}>
-                {check && <i className="fas fa-check"/>}
+            <label className={clsx({check: isCheck})} htmlFor={id}>
+                {isCheck && <i className="fas fa-check"/>}
             </label>
         </div>
     );
