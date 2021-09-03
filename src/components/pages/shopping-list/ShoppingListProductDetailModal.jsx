8577594e4850852fb3bdd57e9c9fd87f4d060b8a
Nicolás Marulanda P <nicomaru@outlook.com>
2021-09-03 23:50:03 +0200

    Nuevo componente ShoppingListProductDetailModal - Muestra en una ventana modal, la información del producto existente en una lista.

---
+++ src/components/pages/shopping-list/ShoppingListProductDetailModal.jsx
@@ -0,0 +1,44 @@
+import Modal from "../../common/modal/Modal";
+import Button from "../../common/button/Button";
+import $ from 'jquery';
+
+const ShoppingListProductDetailModal = ({productDetailModalId, productShoppingList}) => {
+
+    const {product, unit_type, units_per_product, total_calories, total_price} = productShoppingList;
+
+    const closeModalHandle = () => {
+        $(`#${productDetailModalId}`).modal('hide');
+    }
+
+    return (
+        <Modal id={productDetailModalId}>
+            <Modal.Header>
+                <Modal.Header.Title>
+                    {product.name}
+                </Modal.Header.Title>
+                <Button className="close" onClick={closeModalHandle}>
+                    <i className="fas fa-times"/>
+                </Button>
+            </Modal.Header>
+            <Modal.Body className="p-0">
+                <div className="d-flex p-3 border-bottom">
+                    <div className="flex-grow-1">
+                        {total_price} € <small className="text-muted">{product.price} €</small>
+                    </div>
+                    <div className="flex-grow-1">
+                        {total_calories}
+                        <small className="text-muted"> {product.calories}</small>
+                    </div>
+                    <div>
+                        <span className="badge badge-info">
+                            {units_per_product} {unit_type.name}
+                        </span>
+                    </div>
+                </div>
+                <img src={product.img_url} alt={product.name} className="img-fluid img-rounded"/>
+            </Modal.Body>
+        </Modal>
+    );
+}
+
+export default ShoppingListProductDetailModal;
