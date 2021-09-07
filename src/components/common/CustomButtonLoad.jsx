4c351ab963cd59598efad28d903ceb0b94372f1d
Nicolás Marulanda P <nicomaru@outlook.com>
2021-09-07 02:23:14 +0200

    CustomButtonLoad - Botón con el texto Cargar más - Es usado en reemplazo de la paginación.

---
+++ src/components/common/CustomButtonLoad.jsx
@@ -0,0 +1,31 @@
+import Button from "./button/Button";
+import {useEffect, useState} from "react";
+
+const CustomButtonLoad = ({onClick, metaPage}) => {
+
+    const [render, setRender] = useState(false);
+
+    useEffect(() => {
+        setRender(canRender());
+    }, [metaPage]);
+
+    const canRender = () => {
+        const {current_page, last_page} = metaPage;
+
+        return current_page < last_page;
+    }
+
+    return (
+        <>
+            {
+                render && (
+                    <Button className="btn-block mb-3" variant="default" onClick={onClick}>
+                        <i className="fas fa-spinner pr-1"/>
+                        Cargar más
+                    </Button>)
+            }
+        </>
+    );
+};
+
+export default CustomButtonLoad;
