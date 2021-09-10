70d4b9ff8780ded0474ec43caaecad10b3697b10
Nicolás Marulanda P <nicomaru@outlook.com>
2021-09-11 01:03:13 +0200

    Componente Form - FormControl - Se agrega la propiedad "className".

--- src/components/common/form/FormControl.jsx
+++ src/components/common/form/FormControl.jsx
@@ -1,10 +1,12 @@
-const FormControl = ({type, name, id, placeholder, value, onChange, autoComplete, disabled}) => {
+import clsx from "clsx";
+
+const FormControl = ({type, name, id, placeholder, value, onChange, autoComplete, disabled, className}) => {
     return (
         <input autoComplete={autoComplete}
                type={type}
                value={value}
                onChange={onChange}
-               className="form-control"
+               className={clsx("form-control", className)}
                name={name}
                id={id}
                placeholder={placeholder}
