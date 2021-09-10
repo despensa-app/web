710dd39c9570442664a454530d014a90ba547383
Nicolás Marulanda P <nicomaru@outlook.com>
2021-09-11 01:05:53 +0200

    Componente Modal - Se agregan las propiedades "backdrop" y "keyboard".

--- src/components/common/modal/Modal.jsx
+++ src/components/common/modal/Modal.jsx
@@ -3,9 +3,16 @@ import ModalBody from "./ModalBody";
 import ModalHeaderTitle from "./ModalHeaderTitle";
 import ModalFooter from "./ModalFooter";
 
-const Modal = ({children, id}) => {
+const Modal = ({children, id, backdrop = true, keyboard = true}) => {
+
     return (
-        <div className="modal fade" id={id} tabIndex="-1" aria-hidden="true">
+        <div
+            className="modal fade"
+            id={id}
+            tabIndex="-1"
+            aria-hidden="true"
+            data-backdrop={backdrop}
+            data-keyboard={keyboard}>
             <div className="modal-dialog">
                 <div className="modal-content">
                     {children}
