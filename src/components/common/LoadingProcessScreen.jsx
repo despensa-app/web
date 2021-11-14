c098331310674a8b529382523331b43df68dfbfb
Nicolás Marulanda P <nicomaru@outlook.com>
2021-11-14 02:25:36 +0100

    Componente LoadingProcessScreen - Ya no recibe parámetros y ahora hace uso del hook "useIsLoadingProcessScreen"

--- src/components/common/LoadingProcessScreen.jsx
+++ src/components/common/LoadingProcessScreen.jsx
@@ -1,13 +1,18 @@
 import {ProgressSpinner} from 'primereact/progressspinner';
 import clsx from "clsx";
+import {useIsLoadingProcessScreen} from "../../hooks/useLoadingProcessScreen";
 
-const LoadingProcessScreen = ({isLoading = false}) => (
-    <div id="loading-process-screen" className={clsx("full-screen", {"d-block": isLoading}, {"d-none": !isLoading})}>
-        <div className="full-screen-container">
-            <ProgressSpinner/>
+const LoadingProcessScreen = () => {
+    const isLoading = useIsLoadingProcessScreen();
+
+    return (
+        <div id="loading-process-screen" className={clsx("full-screen", {"d-block": isLoading}, {"d-none": !isLoading})}>
+            <div className="full-screen-container">
+                <ProgressSpinner/>
+            </div>
+            <div className="full-screen-background"/>
         </div>
-        <div className="full-screen-background"/>
-    </div>
-);
+    );
+};
 
 export default LoadingProcessScreen
