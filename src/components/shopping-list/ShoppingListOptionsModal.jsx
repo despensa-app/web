6efdc456bea2e43fbac5bb46624de0461e6ea0c3
Nicolás Marulanda P <nicomaru@outlook.com>
2021-11-21 01:48:59 +0100

    Componentes ShoppingListOptionsModal - Se implementa el componente "NavTabs".

--- src/components/shopping-list/ShoppingListOptionsModal.jsx
+++ src/components/shopping-list/ShoppingListOptionsModal.jsx
@@ -1,31 +1,26 @@
 import Modal from "../common/modal/Modal";
+import NavTabs from "../common/nav-tabs/NavTabs";
 
 const ShoppingListOptionsModal = ({modalId}) => {
     return (
         <Modal id={modalId}>
             <Modal.Body className="p-0">
-                <ul className="nav nav-tabs" role="tablist">
-                    <li className="nav-item" role="presentation">
-                        <a className="nav-link border-radius-0 active" id="filter-tab" data-toggle="tab" href="#filter" role="tab" aria-controls="filter" aria-selected="true">
-                            <i className="fas fa-filter pr-1"/>
-                            Filtrar
-                        </a>
-                    </li>
-                    <li className="nav-item" role="presentation">
-                        <a className="nav-link border-radius-0" id="sorting-tab" data-toggle="tab" href="#sorting" role="tab" aria-controls="sorting" aria-selected="false">
-                            <i className="fas fa-sort pr-1"/>
-                            Ordenar
-                        </a>
-                    </li>
-                    <li className="nav-item" role="presentation">
-                        <a className="nav-link border-radius-0" id="options-tab" data-toggle="tab" href="#options" role="tab" aria-controls="options" aria-selected="false">
-                            <i className="fas fa-th-large pr-1"/>
-                            Opciones
-                        </a>
-                    </li>
-                </ul>
-                <div className="tab-content">
-                    <div className="tab-pane fade show active" id="filter" role="tabpanel" aria-labelledby="filter-tab">
+                <NavTabs>
+                    <NavTabs.Item active id="filter">
+                        <i className="fas fa-filter pr-1"/>
+                        Filtrar
+                    </NavTabs.Item>
+                    <NavTabs.Item id="sorting">
+                        <i className="fas fa-sort pr-1"/>
+                        Ordenar
+                    </NavTabs.Item>
+                    <NavTabs.Item id="options">
+                        <i className="fas fa-th-large pr-1"/>
+                        Opciones
+                    </NavTabs.Item>
+                </NavTabs>
+                <NavTabs.Content>
+                    <NavTabs.Content.Pane active id="filter">
                         <div className="list-group list-group-flush">
                             <button type="button" className="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
                                 <i className="fas fa-tags w-fixed-25"/>
@@ -48,8 +43,8 @@ const ShoppingListOptionsModal = ({modalId}) => {
                                 <i className="fas fa-angle-right"/>
                             </button>
                         </div>
-                    </div>
-                    <div className="tab-pane fade" id="sorting" role="tabpanel" aria-labelledby="sorting-tab">
+                    </NavTabs.Content.Pane>
+                    <NavTabs.Content.Pane id="sorting">
                         <div className="list-group list-group-flush">
                             <button type="button" className="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
                                 Fecha de actualización
@@ -64,8 +59,8 @@ const ShoppingListOptionsModal = ({modalId}) => {
                                 <i className="fas fa-long-arrow-alt-down"/>
                             </button>
                         </div>
-                    </div>
-                    <div className="tab-pane fade" id="options" role="tabpanel" aria-labelledby="options-tab">
+                    </NavTabs.Content.Pane>
+                    <NavTabs.Content.Pane id="options">
                         <div className="list-group list-group-flush">
                             <button type="button" className="list-group-item list-group-item-action">
                                 <i className="fas fa-clone w-fixed-25"/>
@@ -76,8 +71,8 @@ const ShoppingListOptionsModal = ({modalId}) => {
                                 Eliminar lista
                             </button>
                         </div>
-                    </div>
-                </div>
+                    </NavTabs.Content.Pane>
+                </NavTabs.Content>
             </Modal.Body>
         </Modal>
     );
