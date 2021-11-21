b1259ba3f9c17f73929011e713ee58fdebad93ce
Nicolás Marulanda P <nicomaru@outlook.com>
2021-11-21 02:08:11 +0100

    Componentes ShoppingListOptionsModal - Se reemplazan los "list group" por su componente correspondiente.

--- src/components/shopping-list/ShoppingListOptionsModal.jsx
+++ src/components/shopping-list/ShoppingListOptionsModal.jsx
@@ -1,5 +1,6 @@
 import Modal from "../common/modal/Modal";
 import NavTabs from "../common/nav-tabs/NavTabs";
+import ListGroup from "../common/list-group/ListGroup";
 
 const ShoppingListOptionsModal = ({modalId}) => {
     return (
@@ -21,56 +22,65 @@ const ShoppingListOptionsModal = ({modalId}) => {
                 </NavTabs>
                 <NavTabs.Content>
                     <NavTabs.Content.Pane active id="filter">
-                        <div className="list-group list-group-flush">
-                            <button type="button" className="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+                        <ListGroup variant="flush">
+                            <ListGroup.Item action
+                                            className="d-flex justify-content-between align-items-center">
                                 <i className="fas fa-tags w-fixed-25"/>
                                 <span className="flex-grow-1">Categorías</span>
                                 <i className="fas fa-angle-right"/>
-                            </button>
-                            <button type="button" className="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+                            </ListGroup.Item>
+                            <ListGroup.Item action
+                                            className="d-flex justify-content-between align-items-center">
                                 <i className="fas fa-calendar-alt w-fixed-25"/>
                                 <span className="flex-grow-1">Fecha de creación</span>
                                 <i className="fas fa-angle-right"/>
-                            </button>
-                            <button type="button" className="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+                            </ListGroup.Item>
+                            <ListGroup.Item action
+                                            className="d-flex justify-content-between align-items-center">
                                 <i className="fas fa-calendar-alt w-fixed-25"/>
                                 <span className="flex-grow-1">Fecha de actualización</span>
                                 <i className="fas fa-angle-right"/>
-                            </button>
-                            <button type="button" className="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+                            </ListGroup.Item>
+                            <ListGroup.Item action
+                                            className="d-flex justify-content-between align-items-center">
                                 <i className="fas fa-star w-fixed-25"/>
                                 <span className="flex-grow-1">Favoritos</span>
                                 <i className="fas fa-angle-right"/>
-                            </button>
-                        </div>
+                            </ListGroup.Item>
+                        </ListGroup>
                     </NavTabs.Content.Pane>
                     <NavTabs.Content.Pane id="sorting">
-                        <div className="list-group list-group-flush">
-                            <button type="button" className="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+                        <ListGroup variant="flush">
+                            <ListGroup.Item action
+                                            className="d-flex justify-content-between align-items-center">
                                 Fecha de actualización
                                 <i className="fas fa-long-arrow-alt-down"/>
-                            </button>
-                            <button type="button" className="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+                            </ListGroup.Item>
+                            <ListGroup.Item action
+                                            className="d-flex justify-content-between align-items-center">
                                 Fecha de creación
                                 <i className="fas fa-long-arrow-alt-down"/>
-                            </button>
-                            <button type="button" className="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+                            </ListGroup.Item>
+                            <ListGroup.Item action
+                                            className="d-flex justify-content-between align-items-center">
                                 Título
                                 <i className="fas fa-long-arrow-alt-down"/>
-                            </button>
-                        </div>
+                            </ListGroup.Item>
+                        </ListGroup>
                     </NavTabs.Content.Pane>
                     <NavTabs.Content.Pane id="options">
-                        <div className="list-group list-group-flush">
-                            <button type="button" className="list-group-item list-group-item-action">
+                        <ListGroup variant="flush">
+                            <ListGroup.Item action
+                                            className="">
                                 <i className="fas fa-clone w-fixed-25"/>
                                 Duplicar lista
-                            </button>
-                            <button type="button" className="list-group-item list-group-item-action text-danger border-top">
+                            </ListGroup.Item>
+                            <ListGroup.Item action
+                                            className="text-danger border-top">
                                 <i className="fas fa-trash w-fixed-25"/>
                                 Eliminar lista
-                            </button>
-                        </div>
+                            </ListGroup.Item>
+                        </ListGroup>
                     </NavTabs.Content.Pane>
                 </NavTabs.Content>
             </Modal.Body>
